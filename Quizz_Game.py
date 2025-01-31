6f5e752afb00bed3082d51d57675da9793078b13
Shwetha M N <shwetha.m21@gmail.com>
2025-01-31 12:53:42 +0530

    Add files via upload

---
+++ Quizz_Game.py
@@ -0,0 +1,50 @@
+#created  tuples 1. questions and 2. options 3.answers
+questions = ("How many elements are in the periodic table?: ",
+                       "Which animal lays the largest eggs?: ",
+                       "What is the most abundant gas in Earth's atmosphere?: ",
+                       "How many bones are in the human body?: ",
+                       "Which planet in the solar system is the hottest?: ")
+
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+                   ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+                   ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+                   ("A. 206", "B. 207", "C. 208", "D. 209"),
+                   ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+
+answers = ("C", "D", "A", "A", "B")
+guesses = []   #guesses is the list
+score = 0       #In score variable assign the value zero like intialization
+question_num = 0  #In question_num variable assign the value zero like intialization
+
+for question in questions:   #for loop for question is questions print questions
+    print("----------------------")
+    print(question)
+    for option in options[question_num]:  # it will print the options and display in list format
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
+
+print("----------------------")
+print("       RESULTS        ")
+print("----------------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+score = int(score / len(questions) * 100)
+print(f"Your score is: {score}%")
